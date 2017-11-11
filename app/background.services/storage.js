81d1afd79382bf336aa1bb0102310f4ccd241b10
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-11 18:17:39 +0530

    Basic class for Storage

---
+++ app/background.services/storage.js
@@ -0,0 +1,29 @@
+/*
+ Storage class to store the History Data
+ 
+ */
+
+// TODO:: need to decide on what storage is to be used
+// Possible use case of SOLID principle application
+  
+class Store {
+    constructor() {
+
+    }
+
+    store() {
+    //Storing data 
+    }
+
+    get() {
+        //Getting data
+    }
+
+    update(){
+
+    }
+
+    delete(){
+
+    }
+}
